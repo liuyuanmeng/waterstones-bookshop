0d6620713493064e3dab097c8ceaea972c84d8cd
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2022-06-29 20:37:35 +0100

    preparing deploy

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose'
 import express from 'express'
 import router from './config/router.js'
 import 'dotenv/config'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const logger = (req, res, next) => {
   console.log(`🚨 - Incoming request on ${req.method} - ${req.url}`)
@@ -15,6 +20,12 @@ const startServer = async () => {
   app.use(logger)
   app.use(express.json())
   app.use('/api', router)
+
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
 
 
   // app.get('/', (req, res, next) => {
